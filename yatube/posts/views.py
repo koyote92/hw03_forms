775a9d380fdfbd1f2055d198b3661b0144c2573b
Roman Inozemtsev <irs1992@yandex.ru>
2022-12-12 01:10:35 +0500

    OK

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
